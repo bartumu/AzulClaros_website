ab6e2878056971062bc987b4d7a8787665091b5e
Bartumu <benartur.ab@gmail.com>
2024-07-23 16:14:28 +0100

    Criação da Tabelas de Pedido e Suas Viewa

--- Portifolio/models.py
+++ Portifolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from AdminDashboard.models import *
+
 
 # Create your models here.
+
+class Pedido (models.Model):
+    data_pedido = models.DateField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    loja = models.ForeignKey(Loja, on_delete=models.CASCADE, related_name='pedido_loja')
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='func')
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='cli')
+    estado = models.BooleanField()
+    data_entrada = models.DateField()
+    data_saida = models.DateField()
+
+    def __str__(self):
+        return f"Pedido em {self.data_pedido} - Cliente: {self.cliente} - Total: {self.total} - Estado: {self.estado} Na Loja: {self.loja}"
+
+class ItemPedido (models.Model):
+    qtd = models.IntegerField()
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='pedido')
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE, related_name='servico')
+
+    def __str__(self):
+        return f"Pedido: {self.pedido}, Serviço: {self.servico}, Quantidade: {self.qtd}"
+
--- Portifolio/urls.py
+++ Portifolio/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="home"),
-    path('/sobre',views.sobre,name="sobre"),
-    path('/pedidos',views.pedido,name="pedido"),
-    path('/adicionar/pedidos',views.addPedido_view,name="pedidoAdd"),
+    path('sobre',views.sobre,name="sobre"),
+    path('pedidos',views.pedido,name="pedido"),
+    path('adicionar/pedidos',views.addPedido_view,name="pedidoAdd"),
 ]
